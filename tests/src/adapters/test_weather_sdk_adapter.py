6677e4a196c067817dc67ca6788a4c08b9caebc1
Benjamim Francisco <bneto@computacao.cua.ufmt.br>
2024-10-06 17:33:48 -0300

    test: Testing WeatherAdapter when it raises an error

--- tests/src/adapters/test_weather_sdk_adapter.py
+++ tests/src/adapters/test_weather_sdk_adapter.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock
 from src.adapters.weather_sdk_adapter import WeatherAdapter
-from src.adapters.weather_sdk_adapter import WeatherAdapter
+from src.adapters.weather_sdk_adapter import WeatherException
 
 
 
@@ -14,3 +14,11 @@ def test_weather_adapter_initialization_success(mock_weather_sdk):
     adapter = WeatherAdapter(access_key="fake_access_key")
     
     assert adapter.weather == mock_instance
+
+# Testing WeatherAdapter when it raises an error
+@patch('src.adapters.weather_sdk_adapter.WeatherSdk')
+def test_weather_adapter_initialization_failure(mock_weather_sdk):
+    mock_weather_sdk.side_effect = Exception("Initialization error")
+    
+    with pytest.raises(WeatherException, match="Initialization error"):
+        WeatherAdapter(access_key="fake_access_key")
