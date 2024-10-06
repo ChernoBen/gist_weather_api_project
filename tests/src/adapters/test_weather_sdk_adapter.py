c33fa0e48d0f2c0d2a3dbd3731289478ef860027
Benjamim Francisco <bneto@computacao.cua.ufmt.br>
2024-10-06 17:37:26 -0300

    test: Testing getting weather data

--- tests/src/adapters/test_weather_sdk_adapter.py
+++ tests/src/adapters/test_weather_sdk_adapter.py
@@ -22,3 +22,20 @@ def test_weather_adapter_initialization_failure(mock_weather_sdk):
     
     with pytest.raises(WeatherException, match="Initialization error"):
         WeatherAdapter(access_key="fake_access_key")
+
+# Testing getting weather data
+@patch('src.adapters.weather_sdk_adapter.WeatherSdk')
+def test_get_weather_success(mock_weather_sdk):
+    mock_instance = mock_weather_sdk.return_value
+    mock_instance.get_weather_data.return_value = {
+        "city": "Itajai",
+        "clima": "Clear",
+        "temp": 25
+    }
+    
+    adapter = WeatherAdapter(access_key="fake_access_key")
+    result = adapter.get_weather(city="Itajai", country="br")
+    
+    assert result["city"] == "Itajai"
+    assert result["clima"] == "Clear"
+    assert result["temp"] == 25
