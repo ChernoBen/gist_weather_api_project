32a5ce55f99669d3ccf925b0d51a50584191ca68
Benjamim Francisco <bneto@computacao.cua.ufmt.br>
2024-10-07 10:42:22 -0300

    test: Testing if add_comment_to_gist method adds a comment to a gist and it return an url

---
+++ tests/src/adapters/test_pygithub_adapter.py
@@ -0,0 +1,18 @@
+import pytest
+from unittest.mock import patch
+from src.adapters.pygithub_adapter import PyGitHubAdapter
+from src.adapters.exceptions.pygithub_exceptions import PyGitHubException
+
+# Testing if add_comment_to_gist method adds a comment to a gist and it return an url
+@patch('src.adapters.pygithub_adapter.Github')
+def test_add_comment_success(mock_github):
+    mock_gist = mock_github.return_value.get_gist.return_value
+    mock_gist.html_url = "https://gist.github.com/fake_gist_url"
+    
+    adapter = PyGitHubAdapter(access_token="fake_token")
+    url = adapter.add_comment_to_gist("This is a test comment")
+    
+    assert url == "https://gist.github.com/fake_gist_url"
+    mock_gist.create_comment.assert_called_once_with("This is a test comment")
+
+
