a0d2d6b744bf9bd6a3cd5783e2b69e7f4cc1bff2
Benjamim Francisco <bneto@computacao.cua.ufmt.br>
2024-10-07 12:19:56 -0300

    test: Testing if controller raises an error if an invalid input was passed

--- tests/src/controllers/test_gist_controller.py
+++ tests/src/controllers/test_gist_controller.py
@@ -17,3 +17,16 @@ def test_create_gist_success(mock_gist_service):
 
     assert response.status_code == 200
     assert response.json() == {"url": "http://gist.url"}
+    
+
+# Testing if controller raises an error if an invalid input was passed
+@patch('src.services.gist_service.GistService')
+def test_create_gist_missing_city(mock_gist_service):
+    mock_gist_instance = mock_gist_service.return_value
+    mock_gist_instance.create_new_gist.return_value = "http://gist.url"
+    
+    with pytest.raises(HTTPException) as exc_info:
+        client.post("/gist/", json={"city": "", "country": "br"})
+
+    assert exc_info.value.status_code == 400
+    assert exc_info.value.detail == "O campo 'city' não pode estar vazio."
